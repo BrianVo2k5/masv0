d0dc237a23daa1532e7bf21b3389bffc3f53904a
mitternachtslaeufer <vgu.longvutrinh@gmail.com>
2026-03-29 13:53:56 +0700

    Fixes!

---
+++ KivyMD fixes for uv (Linux & MacOS).sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+echo "Building KivyMD wheel..."
+pip wheel kivymd==1.2.0 --no-deps
+
+echo "Installing KivyMD from binary..."
+uv pip install kivymd==1.2.0 --only-binary kivymd
+
+echo "Installing the rest of the project dependencies..."
+uv pip install -e .
