3dd4abfcdc0a18a2375f221637cc244cabae36dc
Đăng Khoa Võ (Brian) <143578567+BrianVo2k5@users.noreply.github.com>
2026-06-11 19:00:20 +0700

    new stuff ig

--- Train/export_model.py
+++ Train/export_model.py
@@ -14,8 +14,8 @@ log = logging.getLogger(__name__)
 
 BASE_MODEL_ID = "allenai/led-base-16384"
 
-DEFAULT_CHECKPOINT = Path(__file__).parent / "runs/led-lora-cnn/checkpoint-3400"
-DEFAULT_OUTPUT = Path(__file__).parent / "runs/model.pt"
+DEFAULT_CHECKPOINT = Path(__file__).parent / "runs/bart-lora-xsum/checkpoint-7200"
+DEFAULT_OUTPUT = Path(__file__).parent / "runs/model_bart-xsum.pt"
 
 
 def main():
